44fa585db3b2c42b947b3eeda33505d1e797591a
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-02-19 17:04:27 +0600

    onclick even handling

--- js/event.js
+++ js/event.js
@@ -1,1 +1,21 @@
 console.log("external file");
+//option 2 even handling
+  function makeyellow(){
+            document.body.style.backgroundColor ='yellow';
+        }
+        function makered(){
+            document.body.style.backgroundColor ='red';
+        }
+// option 3 get element by id and then set onclick
+   const btnMakeBlue=document.getElementById('btn-make-blue');
+    //  console.log(btnMakeBlue);
+     btnMakeBlue.onclick=function makeblue(){
+        document.body.style.backgroundColor ='blue';
+     }
+
+     // option 3 different version
+      const btnmakePurple =document.getElementById('btn-make-purple')
+     btnmakePurple .onclick = makepurple;
+     function makepurple (){
+     document.body.style.backgroundColor ='purple';
+     }
